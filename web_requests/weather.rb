29e87051ef314c906fea4b3bbcc1d38eb16035a6
Carlos Aguirre <craguirre952@gmail.com>
2021-06-10 20:42:06 -0400

    access current forecast

--- web_requests/weather.rb
+++ web_requests/weather.rb
@@ -4,6 +4,9 @@ weather = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=chicago&un
 
 weather = weather.parse(:json)
 
-# displya current forecast
-
+# display current forecast
+p city_name = weather["name"]
 p temperature_min = weather["main"]["temp_min"]
+p temperature_max = weather["main"]["temp_max"]
+p description = weather["weather"][0]["description"]
+p wind_speed = weather["wind"]["speed"]
