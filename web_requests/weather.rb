66a9dedf3b2254e9d52feed27ea23a2bd41a1535
Carlos Aguirre <craguirre952@gmail.com>
2021-06-10 20:37:13 -0400

    display current temp min

--- web_requests/weather.rb
+++ web_requests/weather.rb
@@ -1,5 +1,9 @@
 require "http"
 
-response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
+weather = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
 
-pp response.parse(:json)
+weather = weather.parse(:json)
+
+# displya current forecast
+
+p temperature_min = weather["main"]["temp_min"]
